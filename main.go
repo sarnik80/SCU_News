b97edc98d657e2c5951f34fba122e8eaa42d75e2
sarink80 <sarah80nik@gmail.com>
2022-07-28 15:56:42 +0430

    added some function to send request to  the SCU site and get data from response body

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
 type Feed struct {
 	XMLName   xml.Name
@@ -14,4 +20,41 @@ type Entry struct {
 	Author      string
 	PublishedAt string
 	Summary     string
+}
+
+const (
+	RSSURL = "https://scu.ac.ir/%D8%B5%D9%81%D8%AD%D9%87-%D8%A7%D8%B5%D9%84%DB%8C/-/asset_publisher/HDov2wBiUjjC/rss?p_p_cacheability=cacheLevelPage"
+)
+
+func sendReqToSCU() *http.Response {
+
+	resp, err := http.Get(RSSURL)
+
+	simpleHandelError(err)
+	return resp
+
+}
+
+func readByteDataFromSCU() []byte {
+
+	resp := sendReqToSCU()
+	byteDate, err := ioutil.ReadAll(resp.Body)
+
+	simpleHandelError(err)
+
+	return byteDate
+
+}
+
+func simpleHandelError(err error) {
+
+	if err != nil {
+		fmt.Println("err : ", err)
+		os.Exit(1)
+	}
+
+}
+
+func main() {
+
 }
